1d6c93acd153c0cdf3453e03e29a4e6d9a18f223
demo <demo@localhost.localdomain>
2016-09-27 09:42:01 -0700

    prepare program update

---
+++ bench/prepare1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function print_settings {
+#	egrep -r "int nMax = " *
+	egrep -r "int vectorSize = " *
+}
+
+function prepare_vec {
+	p=$1	
+	vec=$2
+	src=${p}Vec
+	dst=${p}Vec$2
+	cp -r $src $dst
+	f=$(egrep -r "vectorSize = " $dst | cut -d: -f1)
+	{
+		rm "$f"
+		sed -E -e "s/vectorSize = [[:digit:]]+/vectorSize = $vec/" >"$f"
+	} <"$f"	
+}
+
+function prepare_vec_lmem {
+	p=$1	
+	vec=$2
+	src=${p}VecLMem
+	dst=${p}Vec$2LMem
+	cp -r $src $dst
+	f=$(egrep -r "vectorSize = " $dst | cut -d: -f1)
+	{
+		rm "$f"
+		sed -E -e "s/vectorSize = [[:digit:]]+/vectorSize = $vec/" >"$f"
+	} <"$f"	
+}
+
+cd ..
+
+
+#for p in Basic; do
+#	prepare_vec_lmem $p 96
+#	prepare_vec_lmem $p 192
+#	prepare_vec_lmem $p 384
+#done
+
+for p in Transposed; do
+	prepare_vec_lmem $p 48
+	prepare_vec_lmem $p 96
+done
+
+for p in Tiled; do
+	prepare_vec_lmem $p 48
+done
+
+exit 0
